072d08ee44037ef7f9978f95bfd3226bfd36ef9f
andrew <ap@ampiusa.com>
2021-03-20 19:47:41 -0700

    timer updated

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+ 
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -71,7 +71,7 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+
   User.findOne({
     where: {
       email: req.body.email
